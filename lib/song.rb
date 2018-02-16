0ea4ac393bf0d26eb878de35ff577f0a569f5369
Neil Seejoor <nvseejoor@gmail.com>
2018-02-16 22:18:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,18 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-
-  def initialize
+  @@genre = []
+  def initialize(name, artist, genre)
     @@count += 1
+    @@genre << genre
+    
   end
 
   def self.count
     @@count
+  end
+
+  def self.genres
+    @genre
   end
 end
